b75539080d49e3e0cbbb3ebb4ec03f9e23bd8d78
Al azery <81170076+AlAzery@users.noreply.github.com>
2021-10-02 21:39:15 +0700

    add script.js

---
+++ assets/project/project1/script.js
@@ -0,0 +1,33 @@
+let generateBtn = document.getElementById("generate-btn");
+let copyBtn = document.getElementById("copy-btn");
+let outputColor = document.getElementById("output-color");
+let outputCode = document.getElementById("output-code");
+let hexString = "0123456789abcdef";
+let randomColor = () => {
+    let hexCode = "#";
+    for( i=0; i<6; i++){
+        hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+    }
+    return hexCode;
+}
+
+let generateGrad = () => {
+    let colorOne = randomColor();
+    let colorTwo = randomColor();
+    let angle = Math.floor(Math.random() * 360);
+    outputColor.style.background = `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
+    outputCode.value = `background: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo});`;
+}
+
+copyBtn.addEventListener("click", () => {
+    outputCode.select();
+    document.execCommand("copy");
+    document.getElementById("alert_C").style.display="block";
+    setTimeout(function() {
+        document.getElementById("alert_C").style.display="none";
+    }, 2000);
+    
+});
+
+generateBtn.addEventListener("click", generateGrad);
+window.onload = generateGrad;
